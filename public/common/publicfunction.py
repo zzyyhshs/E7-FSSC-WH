8b2b4f34834c50e8bade2423b031b4470950d99c
zhuyao <zhuy@yuanian.com>
2019-01-23 18:05:16 +0800

    修改一些文件

--- public/common/publicfunction.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-
-from config import globalparam
-
-
-# 截图放到report下的img目录下
-def get_img(dr, filename):
-    path = globalparam.img_path + '\\' + filename
-    dr.take_screenshot(path)
